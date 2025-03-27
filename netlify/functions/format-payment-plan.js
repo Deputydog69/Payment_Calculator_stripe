d61baa1818f20f79f497454316e6bb20635fce2c
Deputydog69 <thegarn24@gmail.com>
2025-03-27 21:57:26 +1300

    Update format-payment-plan.js

--- netlify/functions/format-payment-plan.js
+++ netlify/functions/format-payment-plan.js
@@ -30,7 +30,7 @@ exports.handler = async (event) => {
     }
 
     // Accept both real and escaped newlines
-    const lines = rawPlan.split(/\\n|\n|\r|\r\n/g);
+    const lines = rawPlan.split(/\n|\n|\r|\r\n/g);
 
     const firstLine = lines[0]?.match(/£([\d.]+) on (\d{2}-\d{2}-\d{4})/);
     const lastLine = lines[lines.length - 1]?.match(/(\d{2}-\d{2}-\d{4})/);
@@ -45,7 +45,7 @@ exports.handler = async (event) => {
 
     const jotformBase = "https://form.jotform.com/250839206727058";
     const allParams = new URLSearchParams({
-      plan14: rawPlan.replace(/£/g, '&pound;').replace(/ /g, '%20'),
+      plan14: rawPlan.replace(/£/g, '&pound;').replace(/ /g, '+'),
       user_name: userName,
       email,
       user_id: userId,
